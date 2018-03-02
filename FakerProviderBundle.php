3f74969d6b3bd1c68ff2eb55ddbee0d27dc6b0bb
emilien puget <puget.e@gmail.com>
2018-03-02 17:02:22 +0100

    SymfonyPasswordProvider

---
+++ FakerProviderBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace EmilienPuget\FakerProvider;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class FakerProviderBundle
+ * @package FakerProvider
+ */
+class FakerProviderBundle extends Bundle
+{
+
+}
---
+++ Provider/SymfonyPasswordProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace EmilienPuget\FakerProvider\Provider;
+
+use Faker\Generator;
+use Faker\Provider\Base;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+/**
+ * Class SymfonyPasswordProvider
+ * @package FakerProvider\Provider
+ */
+final class SymfonyPasswordProvider extends Base
+{
+    /** @var EncoderFactoryInterface */
+    private $encoderFactory;
+
+    /**
+     * {@inheritdoc}
+     * @param EncoderFactoryInterface $encoderFactory
+     */
+    public function __construct(Generator $generator, EncoderFactoryInterface $encoderFactory)
+    {
+        parent::__construct($generator);
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    /**
+     * @param string      $userClass
+     * @param string      $plainPassword
+     * @param string|null $salt
+     *
+     * @return string
+     */
+    public function symfonyPassword(string $userClass, string $plainPassword, string $salt = null): string
+    {
+        $password = $this->encoderFactory->getEncoder($userClass)->encodePassword($plainPassword, $salt);
+
+        return $this->generator->parse($password);
+    }
+}
